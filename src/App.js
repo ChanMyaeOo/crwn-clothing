59a30a88bb13aa5288a486444af2b26205ff7126
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-03-02 12:56:24 +0630

    updating shop page with collection-preview and collection-item

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/hompage/homepage.component";
-
-const hatsPage = props => {
-  console.log(props);
-  return (
-    <div>
-      <h1>HATS PAGE </h1>
-    </div>
-  );
-};
-
-const hatsDetail = props => {
-  return <h1>Hats Detail {props.match.params.hatsId}</h1>;
-};
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={hatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
